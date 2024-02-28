fixed normalization setting, preprocess one-hot encoding, too

hebby.py
@@ -4,20 +4,22 @@ from vis import register_hooks, save_model_data, visualize_model_data, visualize
 import wandb
 import matplotlib.pyplot as plt
 from preprocess import load_and_preprocess_data
-from utils import randomTrainingExample, timeSince, idx_to_char, n_characters
+from utils import randomTrainingExample, timeSince, str2bool, idx_to_char, n_characters
 import time
 import math
 import argparse
 
-def train(line_tensor, rnn, config, state):
+def train(line_tensor, onehot_line_tensor, rnn, config, state):
     hidden = rnn.initHidden()
     losses, og_losses, reg_losses = [], [], []
     l2_reg, output = None, None
 
-    for i in range(line_tensor.size()[0] - 1):
+    for i in range(onehot_line_tensor.size()[0] - 1):
         l2_reg = None  # Reset L2 regularization term for each character
+
+        hot_input_char_tensor = onehot_line_tensor[i].unsqueeze(0)
         # Convert the current character to a one-hot encoded tensor
-        hot_input_char_tensor = torch.nn.functional.one_hot(line_tensor[i], num_classes=n_characters).type(torch.float).unsqueeze(0)
+        # hot_input_char_tensor = torch.nn.functional.one_hot(line_tensor[i], num_classes=n_characters).type(torch.float).unsqueeze(0)
 
         with torch.no_grad():  # Disable gradient calculations
             # Forward pass through the RNN
@@ -50,7 +52,7 @@ def train(line_tensor, rnn, config, state):
 
         if (state["training_instance"] % config["save_frequency"] == 0 and state['training_instance'] != 0):
             # Save the model and activations periodically
-            save_model_data(rnn, state["activations"], state["training_instance"])
+            save_model_data(rnn, state["activations"], state["training_instance"], config['track'])
 
         state['training_instance'] += 1
 
@@ -74,7 +76,8 @@ def main():
     parser.add_argument('--print_freq', type=int, default=50, help='Frequency of printing training progress')
     parser.add_argument('--plot_freq', type=int, default=20, help='Frequency of plotting training loss')
     parser.add_argument('--update_rule', type=str, default='damage', help='How to update weights.')
-    parser.add_argument('--normalize', type=bool, default=True, help='Whether to normalize the weights.')
+    parser.add_argument('--normalize', type=str2bool, nargs='?', const=True, default=True, help='Whether to normalize the weights.')
+    parser.add_argument('--track', type=str2bool, nargs='?', const=True, default=True, help='Whether to track progress online.')
     
     # Add other parameters as needed
     args = parser.parse_args()
@@ -88,19 +91,22 @@ def main():
         "l2_lambda": 0.000,  # Example static hyperparameter
         "n_hidden": args.hidden_size,
         "n_layers": args.num_layers,
+        "track": args.track,
     }
-    # wandb initialization
-    wandb.init(project="hebby", config={
-        "learning_rate": args.learning_rate,
-        "architecture": "crazy hebbian thing",
-        "dataset": "TinyStories",
-        "epochs": 1,
-        "stochasticity": args.stochasticity,
-        "imprint_rate": args.imprint_rate,
-        "len_reward_history": args.len_reward_history,
-        "update_rule": args.update_rule,
-        "normalize": args.normalize
-    })
+    print(args.track)
+    if args.track:
+        # wandb initialization
+        wandb.init(project="hebby", config={
+            "learning_rate": args.learning_rate,
+            "architecture": "crazy hebbian thing",
+            "dataset": "TinyStories",
+            "epochs": 1,
+            "stochasticity": args.stochasticity,
+            "imprint_rate": args.imprint_rate,
+            "len_reward_history": args.len_reward_history,
+            "update_rule": args.update_rule,
+            "normalize": args.normalize
+        })
 
     # Load data
     dataloader = load_and_preprocess_data()
@@ -124,8 +130,8 @@ def main():
     start = time.time()
 
     for iter in range(1, args.n_iters + 1):
-        sequence, line_tensor = randomTrainingExample(dataloader)
-        output, loss, og_loss, reg_loss = train(line_tensor, rnn, config, state)
+        sequence, line_tensor, onehot_line_tensor = randomTrainingExample(dataloader)
+        output, loss, og_loss, reg_loss = train(line_tensor, onehot_line_tensor, rnn, config, state)
         # Loss tracking
         current_loss += loss
         if iter % args.print_freq == 0:
@@ -135,13 +141,15 @@ def main():
             target_char = sequence[-1]
             correct = '✓' if predicted_char == target_char else '✗ (%s)' % target_char
             sequence = sequence[-50:]
-            wandb.log({"loss": loss, "correct": correct, "predicted_char": predicted_char, "target_char": target_char, "sequence": sequence})
+            if args.track:
+                wandb.log({"loss": loss, "correct": correct, "predicted_char": predicted_char, "target_char": target_char, "sequence": sequence})
             print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / args.n_iters * 100, timeSince(start), loss, sequence, predicted_char, correct))
         if iter % args.plot_freq == 0:
             all_losses.append(current_loss / args.plot_freq)
             current_loss = 0
 
-    wandb.finish()
+    if args.track:
+        wandb.finish()
 
     # Plotting the Training Loss
     # plt.figure()

preprocess.py
@@ -1,17 +1,24 @@
 from datasets import load_dataset
 import torch.utils.data
-from utils import filter_text, text_to_indices, collate_fn
+from utils import filter_text, text_to_indices, text_to_indices_and_one_hot, collate_fn
 
 # Load dataset
 def load_and_preprocess_data():
     dataset = load_dataset("roneneldan/TinyStories")
+    # dataset.cleanup_cache_files()
     dataset = dataset['train'].select(range(1000))
 
     print("mapping the filter")
     dataset = dataset.map(filter_text, batched=True)
     print("mapping text to indices")
     dataset = dataset.map(text_to_indices, batched=True)
+    print("mapping text to indices, onehotted")
+    dataset = dataset.map(text_to_indices_and_one_hot, batched=True)
     print('preprocessed') 
+
+    # # Inspect a few examples from the dataset
+    # for i in range(3):
+    #     print(dataset[i]['onehot_tensor'])
 
     # Create a DataLoader
     dataloader = torch.utils.data.DataLoader(

utils.py
@@ -1,6 +1,7 @@
 import torch
 import time
 import math
+import argparse
 
 # Your charset
 charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.;:'\"?!\n- "
@@ -16,18 +17,36 @@ def text_to_indices(examples):
     tensors = [torch.tensor([char_to_idx[char] for char in text], dtype=torch.long) for text in examples['text']]
     return {'tensor': tensors}
 
+def text_to_indices_and_one_hot(examples):
+    one_hot_tensors = []
+    for text in examples['text']:
+        indices = [char_to_idx[char] for char in text]
+        one_hot = torch.nn.functional.one_hot(torch.tensor(indices, dtype=torch.long), num_classes=n_characters).type(torch.float)
+        one_hot_tensors.append(one_hot)
+    return {'onehot_tensor': one_hot_tensors}
+
+# def collate_fn(batch):
+#     """ Collate function for DataLoader """
+#     item = batch[0]
+#     text = item['text']
+#     tensor = item['tensor']
+#     tensor = torch.tensor(tensor)
+#     return text, tensor
 def collate_fn(batch):
     """ Collate function for DataLoader """
     item = batch[0]
     text = item['text']
     tensor = item['tensor']
     tensor = torch.tensor(tensor)
-    return text, tensor
+    onehot_tensor = item['onehot_tensor']
+    onehot_tensor = torch.tensor(onehot_tensor)
+    return text, tensor, onehot_tensor
+
 
 def randomTrainingExample(dataloader):
     """ Get a random training example """
-    for text, tensor in dataloader:
-        return text, tensor
+    for text, tensor, onehot_line_tensor in dataloader:
+        return text, tensor, onehot_line_tensor
 
 def timeSince(since):
     """ Calculate elapsed time """
@@ -36,3 +55,13 @@ def timeSince(since):
     m = math.floor(s / 60)
     s -= m * 60
     return '%dm %ds' % (m, s)
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')

vis.py
@@ -26,7 +26,7 @@ def register_hooks(model):
 
     return activations
 
-def save_model_data(model, activations, epoch):
+def save_model_data(model, activations, epoch, track):
     for name, layer in model.named_modules():
         if isinstance(layer, nn.Linear):  # Replace with your layer types if different
             weight_path = os.path.join(save_dir, f'weights_{name}_epoch{epoch}.pt')
@@ -37,7 +37,8 @@ def save_model_data(model, activations, epoch):
     
     # plot to image and log to wandb
     visualize_all_layers_and_save(model, epoch, os.path.join(save_dir, f'visualization_{epoch}.png'))
-    wandb.log({"model_evolution": wandb.Image(os.path.join(save_dir, f'visualization_{epoch}.png'))},commit=False)
+    if track: 
+        wandb.log({"model_evolution": wandb.Image(os.path.join(save_dir, f'visualization_{epoch}.png'))},commit=False)
 
 # Example usage
 # rnn = SimpleRNN(input_size, n_hidden, output_size, 3)